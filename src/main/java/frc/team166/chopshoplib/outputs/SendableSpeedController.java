97af6f0117b4cc624a598ae93296c01f8c145004
Matt Soucy <msoucy@csh.rit.edu>
2018-08-09 21:04:11 -0400

    Document SendableSpeedController

--- src/main/java/frc/team166/chopshoplib/outputs/SendableSpeedController.java
+++ src/main/java/frc/team166/chopshoplib/outputs/SendableSpeedController.java
@@ -4,6 +4,15 @@ import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 
+/**
+ * An object that is both a Sendable and a SpeedController.
+ * 
+ * <p>Provides a single utility method to wrap an appropriate object.
+ * 
+ * <p>This is used in a few select scenarios,
+ * when we want to keep the exact type of an object general,
+ * but at the same time want to access it as one of two disconnected interfaces.
+ */
 public interface SendableSpeedController extends Sendable, SpeedController {
     static <T extends Sendable & SpeedController> SendableSpeedController wrap(T t) {
         return new SendableSpeedController() {
