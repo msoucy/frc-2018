39d907da3991330b1837bfaaa69d8ced655c3bc7
Matt Soucy <msoucy@csh.rit.edu>
2018-08-23 22:26:18 -0400

    Remove prints

--- src/main/java/frc/team166/chopshoplib/RobotUtils.java
+++ src/main/java/frc/team166/chopshoplib/RobotUtils.java
@@ -23,31 +23,25 @@ public final class RobotUtils {
     public static void logTelemetry() {
         final String branch = getResource("branch.txt");
         SmartDashboard.putString("branch", branch);
-        System.err.println("Branch: " + branch);
 
         final String commit = getResource("commit.txt");
         SmartDashboard.putString("commit", commit);
-        System.err.println("Commit: " + commit);
 
         final String changes = getResource("changes.txt");
         SmartDashboard.putString("changes", changes);
-        System.err.println("Changes:\n" + changes);
 
         final String buildtime = getResource("buildtime.txt");
         SmartDashboard.putString("buildtime", buildtime);
-        System.err.println("Build Time: " + buildtime);
     }
 
     private static String getResource(final String path) {
-        String resource;
         try (InputStream stream = RobotUtils.class.getResourceAsStream("/" + path);
                 InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8);
                 BufferedReader bufferedReader = new BufferedReader(reader)) {
-            resource = bufferedReader.lines()
+            return bufferedReader.lines()
                     .collect(Collectors.joining("\n"));
         } catch (IOException e) {
-            resource = "";
+            return "";
         }
-        return resource;
     }
 }
